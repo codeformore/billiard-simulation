993e935821fcdb4bb5d326918f2c9254897989e7
Caden Fischer <cadenfischer@gmail.com>
2024-04-06 15:31:09 -0500

    Add a basic circle shape

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,37 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Billiard Simulation");
 
+    sf::CircleShape shape(50.f);
+    shape.setFillColor(sf::Color(100,250,50));
+
+    // run the program as long as the window is open
     while (window.isOpen())
     {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
         {
+            // "close requested" event: we close the window
             if (event.type == sf::Event::Closed)
-            {
                 window.close();
-            }
         }
 
-        window.clear();
+        //Move Shape
+        shape.move(0.1f,0.1f);
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        // draw everything here...
+        // window.draw(...);
+        window.draw(shape);
+
+        // end the current frame
         window.display();
     }
+
+    return 0;
 }
